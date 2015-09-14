6ca004d4f08d12fb4bfead136320885512e8102d
Max Horn <max@quendi.de>
2015-09-14 18:41:40 +0200

    Clarify that license is GPL v3 (or later)

--- src/orb.c
+++ src/orb.c
@@ -1650,7 +1650,7 @@ StructInitInfo * Init__orb ( void )
  *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 2 of the License, or
+ *  the Free Software Foundation, either version 3 of the License, or
  *  (at your option) any later version.
  *
  *  This program is distributed in the hope that it will be useful,
