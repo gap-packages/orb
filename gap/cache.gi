df6a853eb411ef3b9820a48cedefd7690ba88fe7
Max Horn <max@quendi.de>
2022-06-26 00:34:24 +0200

    http -> https

--- gap/cache.gi
+++ gap/cache.gi
@@ -151,5 +151,5 @@ InstallGlobalFunction( UseCacheObject,
 ##  GNU General Public License for more details.
 ##
 ##  You should have received a copy of the GNU General Public License
-##  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+##  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 ##
--- gap/transform.gd
+++ gap/transform.gd
@@ -33,5 +33,5 @@ DeclareGlobalFunction( "IsInjectiveTransOnList" );
 ##  GNU General Public License for more details.
 ##
 ##  You should have received a copy of the GNU General Public License
-##  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+##  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 ##
